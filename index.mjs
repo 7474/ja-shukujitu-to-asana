052c092405938bba8a6936f1bf63c06057313662
7474 <koudenpa@hatena.ne.jp>
2022-07-15 15:27:34 +0900

    put asana create only

--- index.mjs
+++ index.mjs
@@ -1,21 +1,29 @@
-import ical from 'node-ical';
-
-const events = await ical.async.fromURL('https://calendar.google.com/calendar/ical/ja.japanese%23holiday%40group.v.calendar.google.com/public/basic.ics');
-// // loop through events and log them
-// for (const event of Object.values(events)) {
-//     console.log(
-//         'Summary: ' + event.summary +
-//         '\nDescription: ' + event.description +
-//         '\nStart Date: ' + event.start.toISOString() +
-//         '\n'
-//     );
-// };
+import ical from "node-ical";
+import asana from "asana";
 
 const now = new Date();
-const upsertEvents = Object.values(events)
-    .filter(e => e.start)
-    .filter(e => e.start > now);
 
-upsertEvents.forEach(event => {
-    console.log(event);
-});
+const events = await ical.async.fromURL(
+  "https://calendar.google.com/calendar/ical/ja.japanese%23holiday%40group.v.calendar.google.com/public/basic.ics"
+);
+const upsertEvents = Object.values(events)
+  .filter((e) => e.start)
+  .filter((e) => e.start > now);
+
+const targetProjectIds = process.env.ASANA_PROJECT_IDS.split(",");
+
+const client = asana.Client.create().useAccessToken(process.env.ASANA_TOKEN);
+const me = await client.users.me();
+
+console.log(me);
+
+for (const event of upsertEvents) {
+  console.log(event);
+  const task = await client.tasks.create({
+    name: event.summary,
+    due_at: event.start,
+    projects: targetProjectIds,
+    resource_subtype: 'milestone',
+  });
+  console.log(task);
+}
